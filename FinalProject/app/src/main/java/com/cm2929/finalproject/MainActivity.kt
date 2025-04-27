3307bcd4a17c91148e4bb1e9b3fd68b2c7e6e4fa
Aidan Hebert <102483879+Arh15@users.noreply.github.com>
2025-04-27 15:52:25 -0700

    New main activity

--- FinalProject/app/src/main/java/com/cm2929/finalproject/MainActivity.kt
+++ FinalProject/app/src/main/java/com/cm2929/finalproject/MainActivity.kt
@@ -1,29 +1,38 @@
 package com.cm2929.finalproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.cm2929.finalproject.adapters.PostAdapter
 import com.cm2929.finalproject.databinding.ActivityMainBinding
+import com.cm2929.finalproject.models.Post
+import com.cm2929.finalproject.network.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: SimplePostAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.recyclerViewPosts.layoutManager = LinearLayoutManager(this)
 
-        val samplePosts = listOf(
-            PostItem("Title 1", "Description for item 1"),
-            PostItem("Title 2", "Description for item 2"),
-            PostItem("Title 3", "Description for item 3")
-        )
+        RetrofitInstance.api.getPosts().enqueue(object : Callback<List<Post>> {
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                if (response.isSuccessful) {
+                    val posts = response.body() ?: emptyList()
+                    binding.recyclerViewPosts.adapter = PostAdapter(posts)
+                }
+            }
 
-        adapter = SimplePostAdapter(samplePosts)
-        binding.recyclerViewPosts.adapter = adapter
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Failed to load posts", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
