5d2c394b0f883e409e23f58e68cbec1ef08eb091
Aidan Hebert <102483879+Arh15@users.noreply.github.com>
2025-04-30 22:12:14 -0700

    Updated Main Activity & others

--- FinalProject/app/src/main/java/com/cm2929/finalproject/SimplePostAdapter.kt
+++ FinalProject/app/src/main/java/com/cm2929/finalproject/SimplePostAdapter.kt
@@ -4,11 +4,25 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cm2929.finalproject.databinding.ItemPostBinding
+import com.cm2929.finalproject.models.Post
 
 data class PostItem(val title: String, val description: String)
 
-class SimplePostAdapter(private var posts: List<PostItem>) : RecyclerView.Adapter<SimplePostAdapter.PostViewHolder>() {
-    inner class PostViewHolder(val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root)
+class PostAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+    private var onItemClickListener: ((Post) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Post) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    class PostViewHolder(private val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: Post, clickListener: ((Post) -> Unit)?) {
+            binding.textViewTitle.text = post.title
+            binding.textViewBody.text = post.body
+            binding.root.setOnClickListener { clickListener?.invoke(post) }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         val binding = ItemPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -16,15 +30,8 @@ class SimplePostAdapter(private var posts: List<PostItem>) : RecyclerView.Adapte
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        val post = posts[position]
-        holder.binding.textViewTitle.text = post.title
-        holder.binding.textViewDescription.text = post.description
+        holder.bind(posts[position], onItemClickListener)
     }
 
     override fun getItemCount(): Int = posts.size
-
-    fun updateData(newPosts: List<PostItem>) {
-        posts = newPosts
-        notifyDataSetChanged()
-    }
 }
