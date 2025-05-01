d33f96e9c48d3e313625ce4b3e226777814fa88e
Aidan Hebert <102483879+Arh15@users.noreply.github.com>
2025-04-30 22:32:15 -0700

    Updated API service

--- FinalProject/app/src/main/java/com/cm2929/finalproject/network/ApiService.kt
+++ FinalProject/app/src/main/java/com/cm2929/finalproject/network/ApiService.kt
@@ -1,10 +1,14 @@
 package com.cm2929.finalproject.network
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import com.cm2929.finalproject.models.Post
 import retrofit2.Call
 
 interface ApiService {
     @GET("posts")
     fun getPosts(): Call<List<Post>>
+
+    @GET("posts/{id}")
+    fun getPost(@Path("id") id: Int): Call<Post> // Fetch a single post by ID
 }
